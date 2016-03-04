9c363bbecba8d6ac1f4e1884b20aaa9ab05f0391
Jason Chan <jasonfchan@gmail.com>
2016-03-03 19:21:40 -0800

    master

---
+++ BlockExtreme/BlockExtreme/Array2D.swift
@@ -0,0 +1,31 @@
+//
+//  Array2D.swift
+//  BlockExtreme
+//
+//  Created by Jason Chan MBP on 2/29/16.
+//  Copyright © 2016 Jason Chan. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    // #2
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        // #3
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    // #4
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
